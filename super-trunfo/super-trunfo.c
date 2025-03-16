6bd94014cd9416eac27f9a2f4068fa83bd544bc5
juliacarv99 <juliacarvalhofig@gmail.com>
2025-03-16 18:32:54 -0300

    Adição dos cálculos
    Aprimoramento do programa:
    - Adicionados cálculos de densidade populacional e PIB per capita.
    - Exibição dos novos dados formatados com duas casas decimais.

--- super-trunfo/super-trunfo.c
+++ super-trunfo/super-trunfo.c
@@ -11,6 +11,8 @@ int main() {
     int  populacao, populacao2;
     int pontos, pontos2;
     float area, pib, area2, pib2;
+    float densidade_populacional, densidade_populacional2;
+    float pib_per_capita, pib_per_capita2;
 
     //entrada e saida de dados carta 1
     printf("carta 1:\n");
@@ -36,6 +38,10 @@ int main() {
     printf("número de pontos turisticos: ");
     scanf("%d", &pontos);
 
+     // Cálculos para a Carta 1
+     densidade_populacional = populacao / area;
+     pib_per_capita = pib / populacao;
+
     // Exibição dos dados da Carta 1
     printf("\nCarta 1\n");
     printf("Estado: %s\n", estado);
@@ -45,6 +51,8 @@ int main() {
     printf("Área %.2f km²: \n", area);
     printf("PIB: %.2f\n", pib);
     printf("Número de Pontos Turísticos: %d\n\n", pontos);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
+    printf("PIB per Capita: %.2f\n\n", pib_per_capita);
 
 
     //entrada e saida de dados carta 2
@@ -72,6 +80,10 @@ int main() {
     printf("número de pontos turisticos: ");
     scanf("%d", &pontos2);
 
+     // Cálculos para a Carta 2
+     densidade_populacional2 = populacao2 / area2;
+     pib_per_capita2 = pib2 / populacao2;
+
     // Exibição dos dados da Carta 2
     printf("\nCarta 2\n");
     printf("Estado: %s\n", estado2);
@@ -81,6 +93,11 @@ int main() {
     printf("Área %.2f km²: \n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);
+    printf("PIB per Capita: %.2f\n\n", pib_per_capita2);
+
+    return 0;
+
 
 
 
